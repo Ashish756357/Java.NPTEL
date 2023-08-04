c9837027b096a6a5a57b3b84be616aa7c9ee3b72
Ashish Raj <126260804+Ashish756357@users.noreply.github.com>
2023-08-04 23:24:00 +0530

    Create partten.java

---
+++ partten.java
@@ -0,0 +1,22 @@
+package test1;
+public class pattern {
+    public static void main(String[] args) {
+        int rows = 6; // Number of rows for the first part of the pattern
+
+        // Print the first part of the pattern
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Print the second part of the pattern
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
